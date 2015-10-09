2d84470997f0c52e9c7100fe8de491897a6dda74
Daniel Calderon <danielcalderon@student.hartnell.edu>
2015-10-09 10:01:03 -0700

    Added names and prototypes and skeleton main to program

---
+++ driver.cpp
@@ -0,0 +1,15 @@
+// Raquel FIgueroa
+// Ali Fenton
+//David jia
+
+char menu( );
+double  climbing(... );
+double scuba(... );
+double skyDive(... );
+double spelunk();
+
+int main()
+{
+    return 0;
+}
+
